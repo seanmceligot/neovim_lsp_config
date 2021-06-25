7974fa3491cbecb62adb1160783f568ab70b0957
Sean McEligot <sean.mceligot@gmail.com>
2021-06-24 20:26:20 -0400

    verical diff, cleanup

--- init.lua
+++ init.lua
@@ -1,14 +1,16 @@
 
 
--- https://github.com/wbthomason/packer.nvim
--- https://github.com/neovim/nvim-lspconfig
--- https://github.com/hrsh7th/nvim-compe
+-- lua/packerinit.lua https://github.com/wbthomason/packer.nvim
+-- lua/lspinit.lua https://github.com/neovim/nvim-lspconfig
+-- lua/compeinit.lua https://github.com/hrsh7th/nvim-compe
 -- https://github.com/neomake/neomake
 -- gd:  goto definiion
 -- gr: list references
 -- K: show hover information
 -- :NeomakeProject cargo
 -- :copen
-require'packerinit'
-require'compeinit'
-require'lspinit'
+require('packerinit')
+require('compeinit')
+require('lspinit')
+
+vim.opt.diffopt = vim.opt.diffopt + "vertical"
